6badc60d5aa8322e4c614bfa5da2905863f8bc3b
Arun M V <arunmv@assyst.in>
2012-11-23 12:28:13 +0530

    Bingo Test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   end
 
   def new
-    #This is very much incorrect and terribly wrong	
+    #This is very much incorrect and terribly wrong indeed	
     @comment=Comment.new
     @comment.post_id=params[:id]
   end
